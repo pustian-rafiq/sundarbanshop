467ae2110195155b940e0bafa23312ee98d349e8
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2024-01-07 10:39:23 +0600

    Design register page and create models

---
+++ sundarbanshop_backend/src/models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: String,
+  address: {
+    name: String,
+    mobileNo: String,
+    houeseNo: String,
+    streetNo: String,
+    landmark: String,
+    city: String,
+    country: String,
+    postalCode: String,
+  },
+  orders: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Order",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
